b369fa66bdabb5e4600d6a9beb3356275fe4c47c
mariotroiano <mariotroiano2@gmail.com>
2023-04-22 12:17:31 -0300

    add payment service refactor

---
+++ src/services/PaymentService.ts
@@ -0,0 +1,68 @@
+import { TableInstance } from './interfaces/TableInstance';
+import { PaymentPayload } from 'services/payloads/PaymentPayload';
+import { ax } from './util';
+import { JsonApiClient } from './client/JsonApiClient';
+
+const client = new JsonApiClient(PaymentPayload, ax, 'payments');
+
+export class PaymentService {
+
+  static getById = async (id: string) => {
+    const result = await client.getById(id);
+    return result;
+  }
+
+  static applyAction = async (id: string, action: string) => {
+    const result = await client.patch(id, { action } );
+    return result;
+  }
+
+  static getByReservationId = async (reservationId: string) => {
+    const params = {
+      filter: { reservation_id: reservationId},
+      include: 'refunds'
+    };
+    const result = await client.getAll(params);
+    return result.entities;
+  };
+
+  static getByGuestId = async (guestId: string) => {
+    const params = { filter: { guest_id: guestId } };
+    const result = await client.getAll(params);
+    return result.entities;
+  };
+
+  static getByOrderId = async (orderId: string) => {
+    const params = { filter: { order_id: orderId } };
+    const result = await client.getAll(params);
+    return result.entities;
+  };
+
+  static createOrUpdateCard = async (entity: PaymentPayload) => {
+    const result = await client.createOrUpdate(entity);
+    return result;
+  }
+
+  static delete = async (id: string) => {
+    const result = await client.delete(id);
+    return result;
+  }
+
+  static find = async (guestId: string, table: TableInstance) => {
+    if (!table.sorting) {
+      table.setters.setSorting({ field: 'created_at', order: 'descend' });
+    }
+    const result = await client.findForTable(table, { guestId });
+    return result
+  }
+
+  static getAll = async (search: string, guestId: string) => {
+    const params = {
+      filter: { guestId, q: { contains: search } },
+      page: { number: 1, size: 15 },
+      sort: '-created_at',
+    }
+    const result = await client.getAll(params);
+    return result.entities
+  }
+}
