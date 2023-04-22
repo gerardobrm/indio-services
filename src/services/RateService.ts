87cf7310fb90507750426a980c185179c72d91be
mariotroiano <mariotroiano2@gmail.com>
2023-04-22 12:34:55 -0300

    add rate service refactor

---
+++ src/services/RateService.ts
@@ -0,0 +1,50 @@
+import { TableInstance } from './interfaces/TableInstance';
+import { RatePayload } from './payloads/RatePayload';
+import { JsonApiClient } from './client/JsonApiClient';
+import { ax } from './util';
+
+const client = new JsonApiClient(RatePayload, ax, 'rates');
+
+export class RateService {
+
+  static getById = async (id: string) => {
+    const result = await client.getById(id);
+    return result;
+  }
+
+  static createOrUpdateCard = async (entity: RatePayload) => {
+    const result = await client.createOrUpdate(entity);
+    return result;
+  }
+
+  static find = async (parkId: string, table: TableInstance) => {
+    if (!table.sorting) {
+      table.setters.setSorting({ field: 'created_at', order: 'descend' });
+    }
+    const result = await client.findForTable(table, { parkId });
+    return result
+  }
+
+  static getAll = async (search: string, parkId: string) => {
+    const params = {
+      filter: { park_id: parkId, q: { contains: search } },
+      page: { number: 1, size: 200 },
+      sort: '-created_at',
+    }
+    const result = await client.getAll(params);
+    return result.entities
+  }
+  
+  static getBySiteTypeId = async (siteTypeId: string, activeOnly: boolean) => {
+    const params = {
+      filter: { site_type_id: siteTypeId },
+      include: 'discounts'
+    };
+    if (activeOnly) {
+      params.filter['active'] = true;
+    }
+    const result = await client.getAll(params);
+    return result.entities;
+  };
+  
+}
